ad8a11d0e067a5dc7fe546fb8e5477487c9e1270
smilee1111 <muskankc2005@gmail.com>
2024-02-06 19:24:40 +0545

    changes made

--- home.py
+++ home.py
@@ -10,8 +10,6 @@ bgimage = PhotoImage(file='C:/Users/Dell/Desktop/files/G/Gharbeti/logo1.png')
 background_label = Label(a, image=bgimage)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 c.pack()
-e1=Label(a,text="Gharbeti")
-e1.place(x=100,y=100)
 # a.iconbitmap("a.ico")
 
 
