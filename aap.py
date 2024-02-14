33b6db2a3a0a8358dc09edc6d7a3c360ea7a4520
Rabin Tamang <147827786+Rabin935@users.noreply.github.com>
2024-02-14 14:19:50 +0545

    Home page new design

--- aap.py
+++ aap.py
@@ -2,12 +2,12 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import ImageTk,Image
 a=Tk()
-# a.iconbitmap("a.ico")
-a.title("GHARBETI")
+a.iconbitmap("a.ico")
+a.title("GHARBETI - Add problem")
 a.geometry("1100x700")
 a.resizable(0,0)
 c = Canvas(a, height=1100, width = 700)
-bgimage = PhotoImage(file='C:/Users/Dell/Desktop/files/G/Gharbeti/logo1.png')
+bgimage = PhotoImage(file='logo1.png')
 background_label = Label(a, image=bgimage)
 background_label.place(x=0, y=30, relwidth=1, relheight=1)
 c.pack()
